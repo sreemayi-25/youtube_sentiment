9b12e6bab0fd6be0c09262bf72c6e18a9724b466
sreemayi-25 <sreemayi25@gmail.com>
2025-06-03 15:47:33 +0530

    Add whitenoise to requirements

--- youtube_sentiment/settings.py
+++ youtube_sentiment/settings.py
@@ -114,12 +114,11 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = "/static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
